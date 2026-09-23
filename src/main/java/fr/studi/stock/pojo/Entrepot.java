0aaef19b8be8c0973007fce0635b123a69410a47
Aurelien Le Roch <aurelienleroch@gmail.com>
2026-09-23 12:06:56 +0800

    Configurer l'application et ajouter les POJO

---
+++ src/main/java/fr/studi/stock/pojo/Entrepot.java
@@ -0,0 +1,12 @@
+package fr.studi.stock.pojo;
+
+import lombok.Data;
+
+@Data
+public class Entrepot {
+
+    private Long entrepot_id; // en base entrepot_id
+    private String adresse;
+    private String nom;
+
+}
---
+++ src/main/java/fr/studi/stock/pojo/Produit.java
@@ -0,0 +1,14 @@
+package fr.studi.stock.pojo;
+
+import lombok.Data;
+
+@Data
+public class Produit {
+
+    private Long produit_id;
+    private String nom;
+    private String description;
+    private Float prix;
+    private Integer quantite;
+
+}
