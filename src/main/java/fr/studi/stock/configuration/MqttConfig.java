06449200043bc4e67c69dab7c12ef20e8ba21d8d
Aurelien Le Roch <aurelienleroch@gmail.com>
2026-09-24 17:24:53 +0800

    update application.yml et création du Bean Mqtt

--- src/main/java/fr/studi/stock/configuration/MqttConfig.java
+++ src/main/java/fr/studi/stock/configuration/MqttConfig.java
@@ -1,4 +1,39 @@
 package fr.studi.stock.configuration;
 
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MqttConfig {
+
+    @Value("${mqtt.broker.url}")
+    private String brokerUrl;
+
+    @Value("${mqtt.client.id}")
+    private String clientId;
+
+    @Value("${mqtt.username}")
+    private String username;
+
+    @Value("${mqtt.password}")
+    private String password;
+
+    @Bean
+    public MqttClient mqttClient() throws MqttException {
+
+        //instanciation du client
+        MqttClient mqttClient = new MqttClient(brokerUrl, clientId, new MemoryPersistence() );
+
+        //argument supplémentaire
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(username);
+        options.setPassword(password.toCharArray());
+        mqttClient.connect(options);
+        return mqttClient;
+    }
 }
